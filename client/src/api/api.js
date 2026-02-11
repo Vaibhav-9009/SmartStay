2a3d2289040f0caffddcf68e6754c669177dfda1
Vaibhav-9009 <vaibhavdwivedi0321@gmail.com>
2026-02-11 15:36:43 +0530

    Prepare frontend for Vercel deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smartstay-352j.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 
 export default API;
